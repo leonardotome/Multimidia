c6e083931ee22b54561494df18cf05542b3f674d
leonardotome <leonardo.eduardo@sptech.school>
2022-05-30 14:24:12 -0300

    Header

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Multimidia
-Site da equipe de Multimídia da Igreja Batista Boas Novas 
+# API Projeto Site - 1º Semestre
+Esta API será utilizada para conectar seu Website do Projeto de PI ao Banco de Dados.
---
+++ src/models/medidaModel.js
@@ -0,0 +1,66 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas(idAquario, limite_linhas) {
+    
+    instrucaoSql = ''
+    
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top ${limite_linhas}
+                        temperatura, 
+                        umidade, 
+                        momento,
+                        CONVERT(varchar, momento, 108) as momento_grafico
+                    from medida
+                    where fk_aquario = ${idAquario}
+                    order by id desc`;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+                        temperatura, 
+                        umidade, 
+                        momento,
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
+                    from medida
+                    where fk_aquario = ${idAquario}
+                    order by id desc limit ${limite_linhas}`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+    
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(idAquario) {
+    
+    instrucaoSql = ''
+    
+    if (process.env.AMBIENTE_PROCESSO == "producao") {       
+        instrucaoSql = `select top 1
+                        temperatura, 
+                        umidade, CONVERT(varchar, momento, 108) as momento_grafico, 
+                        fk_aquario 
+                        from medida where fk_aquario = ${idAquario} 
+                    order by id desc`;
+        
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+                        temperatura, 
+                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+                        fk_aquario 
+                        from medida where fk_aquario = ${idAquario} 
+                    order by id desc limit 1`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
